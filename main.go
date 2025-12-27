140029307da96ffe02824f7fd0c0db6bbafd7b41
coshi <muhammadcoshi.dz@gmail.com>
2025-12-27 12:12:51 +0100

    place holder metadata will change latter but the packaging was tested and it works

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
+	"time"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
+func updateTime(clock *widget.Label) {
+	current_time := time.Now().Format("The current time is: 03:04:05")
+	clock.SetText(current_time)
+}
 func main() {
 	a := app.New()
-	w := a.NewWindow("test window")
-	w.SetContent(widget.NewLabel("hello from go lang"))
-	w.ShowAndRun()
+	w := a.NewWindow("Clock")
+	clock := widget.NewLabel("the current time is")
+	go func() {
+		for range time.Tick(time.Second * 1) {
+			updateTime(clock)
+		}
+	}()
+	w.SetContent(clock)
+	w.SetMaster()
+	w.Show()
+	a.Run()
 }
